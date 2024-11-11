c54beb504da481984664e8dc35ee9def43178f64
Airlangga Pangestu <138566683+airlanggapangestu@users.noreply.github.com>
2024-11-11 12:46:04 +0700

    Update condition.js

--- condition.js
+++ condition.js
@@ -1,12 +1,26 @@
 document.getElementById("orderButton").onclick = function () {
-  let foodPrice = parseInt(document.getElementById("food").value);
-  let drinkPrice = parseInt(document.getElementById("drink").value);
+    let foodPrice = parseInt(document.getElementById("food").value);
+    let drinkPrice = parseInt(document.getElementById("drink").value);
+  
+    let totalPrice = foodPrice + drinkPrice;
+    let discount = 0;
+  
+    if (totalPrice > 100) {
+     discount = totalPrice * 0.1;
+     totalPrice -= discount;
+    }
 
-  let totalPrice = foodPrice + drinkPrice;
+    const resultDiv = document.getElementById("result");
+    if (totalPrice > 0) {
+        resultDiv.innerHTML= ` 
+        Total : $${foodPrice + drinkPrice}<br>
+        Discount : $${discount.toFixed(2)}<br>
+        Final Price : $${totalPrice.toFixed(2)} `;
+    }
+    else {
+        resultDiv.innerHTML = "Please Select both Food and drink"
+    }
 
-  let discount = 0;
+ 
 
-  if (totalPrice > 90) {
-    alert("ok");
-  }
-};
+  };
